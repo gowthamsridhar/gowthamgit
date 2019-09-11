5021bdb4ba922bdf3ad0e47972cb815bc840e3dc
gowthamsridhar <gauthamsri111@gmail.com>
2019-09-11 14:04:38 +0530

    java project added

---
+++ src/CountOfOdd.java
@@ -0,0 +1,15 @@
+
+public class CountOfOdd {
+	public static void main(String[] args) {
+		int count=0;
+		for (int i = 0; i <=100; i++) {
+			if (i%2==1) {
+				count=count+1;
+			}
+		}
+	
+	System.out.println(count);
+	
+	}
+
+}
